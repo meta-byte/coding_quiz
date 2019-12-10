31b53b4bc54ec39e0fcc8787965466e4b0cee90a
Garrett Howard <hylander.garrett@gmail.com>
2019-12-09 18:30:33 -0700

    updated questions in questions.js

---
+++ assets/javascript/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "In JQuery, Which symbol is commonly used?",
+        choices: ["&", "@", "$", "#"],
+        answer: "$"
+    },
+    {
+        title: "For a 'link' tag, which attribute allows you to link an external style sheet?",
+        choices: ["src", "href", "a", "rel"],
+        answer: "href"
+    },
+    {
+        title: "How many units must columns add up to when using Bootsrap?",
+        choices: ["10", "6", "8", "12"],
+        answer: "12"
+    },
+];
