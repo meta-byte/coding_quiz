ad1dd81a444fea7516c912a037770569ebe2c957
Garrett Howard <hylander.garrett@gmail.com>
2019-12-11 16:55:10 -0700

    updated so that scripts load when document is ready

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,11 +1,20 @@
+$(document).ready(function () {
+    // put everything in here
+})
+
 startbutton = document.querySelector('#start')
 
-
-// $.getScript('assets/javascript/questions.js', function () {
-
-// });
 
 $("#start").click(function () {
     startbutton.setAttribute("class", "hide")
     console.log("click")
 });
+
+questions
+
+console.log(questions)
+
+function nextquestion(
+
+)
+
