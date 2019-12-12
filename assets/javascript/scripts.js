b0d58851d7ed8e8d457b7538dad7f16f853300d2
Garrett Howard <hylander.garrett@gmail.com>
2019-12-11 18:23:30 -0700

    created a basic timer function

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -8,13 +8,31 @@ startbutton = document.querySelector('#start')
 $("#start").click(function () {
     startbutton.setAttribute("class", "hide")
     console.log("click")
+    timer()
 });
-
-questions
 
 console.log(questions)
 
-function nextquestion(
 
-)
 
+//Function for the timer
+
+function timer() {
+    var time = document.querySelector("#time")
+    var count = 15
+    time.innerHTML = count
+    var interval = setInterval(function () {
+        count--
+        time.innerHTML = count
+    }, 1000);
+}
+
+
+//Function that updates timer and gets next question
+
+function nextquestion() {
+
+}
+
+//Function that stops the quiz when the time ends and saves the score to storage
+//Get score from storage and append it to high scores page.
