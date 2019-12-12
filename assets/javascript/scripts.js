52f3fd150accd937346839e98e711ded82ed8551
Garrett Howard <hylander.garrett@gmail.com>
2019-12-11 22:33:07 -0700

    questions cycle when answer buttons are clicked and clicking an incorrect answer no detracts from time

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -9,16 +9,18 @@ var buttonBoxEl = document.querySelector('#button-box')
 
 
 $("#start").click(function () {
-    startbutton.setAttribute("class", "hide")
+    // startbutton.setAttribute("class", "hide")
+    $("#button-box").empty();
+    titleEl.setAttribute("class", "hide")
     console.log("click")
     timer()
     nextquestion()
 });
 
+var count = 75
 //Function for the timer
 function timer() {
     var time = document.querySelector("#time")
-    var count = 15
     time.innerHTML = count
     var interval = setInterval(function () {
         count--
@@ -28,22 +30,45 @@ function timer() {
 
 //Function that stops the quiz when the time ends and saves the score to storage
 
-
 //Function that gets next question
+var questionindex = 0
 function nextquestion() {
-    var currentQuestion = questions[0];
+
+    var currentQuestion = questions[questionindex];
+    questionindex++
     console.log(currentQuestion)
     questionEl.innerText = currentQuestion.title
     for (var i = 0; i < currentQuestion.choices.length; i++) {
         var answerButton = document.createElement("button")
-        answerButton.setAttribute("class", "btn")
+        answerButton.setAttribute("class", "answerbtn")
         answerButton.innerHTML = currentQuestion.choices[i]
+        answerButton.setAttribute("data-id", currentQuestion.choices[i])
         buttonBoxEl.appendChild(answerButton);
+
     }
+    $(".answerbtn").click(function () {
+
+        var selected = $(this).attr("data-id")
+        var answer = currentQuestion.answer
+        console.log(answer)
+        console.log(selected)
+
+        if (answer === selected) {
+            $("#button-box").empty();
+            // answerButton.setAttribute("class", " hide")
+            nextquestion()
+
+        }
+        else {
+            $("#button-box").empty();
+            nextquestion()
+            count = count - 15
+            time.innerHTML = count
+        }
+    });
 
 }
 
+//Click answer
 
-
-//Function that stops the quiz when the time ends and saves the score to storage
 //Get score from storage and append it to high scores page.
