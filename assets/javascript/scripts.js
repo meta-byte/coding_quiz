8005de00b0464c03626ef3865d8a47135ee97257
Garrett Howard <hylander.garrett@gmail.com>
2019-12-13 17:52:55 -0700

    changed index.html to be fully dynamic, scores now populate from storage

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,14 +1,18 @@
 $(document).ready(function () {
     var titleEl = document.querySelector('#title')
-    var startbutton = document.querySelector('#start')
     var questionEl = document.querySelector('#quiz-question')
     var buttonBoxEl = document.querySelector('#button-box')
     var formBoxEl = document.querySelector('#form-box')
+    var scoreBoxEl = document.querySelector('#scores-box')
+    var scoreName = document.querySelector('#inputName')
+    var highscoresEl = document.querySelector('#highscores')
     var interval
     var count = 75
     var questionindex = 0
     var score
+    var highscores
 
+    //start quiz
     $("#start").click(function () {
         $("#button-box").empty();
         titleEl.setAttribute("class", "hide")
@@ -23,9 +27,9 @@ $(document).ready(function () {
         time.innerHTML = count
         interval = setInterval(function () {
             if (count === 0) {
-                stopTimer()
                 stopQuiz()
             }
+
             else {
                 count--
                 time.innerHTML = count
@@ -41,9 +45,11 @@ $(document).ready(function () {
 
     //Function that stops quiz and stores time as score
     function stopQuiz() {
+        stopTimer()
         $("#button-box").empty();
         buttonBoxEl.setAttribute("class", "hide")
         formBoxEl.removeAttribute("class", "hide")
+        formBoxEl.setAttribute("class", "col-8 text-center")
         questionEl.innerText = "You completed the quick coding quiz!"
         score = time.innerText
         console.log(score)
@@ -59,7 +65,7 @@ $(document).ready(function () {
             questionEl.innerText = currentQuestion.title
             for (var i = 0; i < currentQuestion.choices.length; i++) {
                 var answerButton = document.createElement("button")
-                answerButton.setAttribute("class", "answerbtn")
+                answerButton.setAttribute("class", "btn answerbtn")
                 answerButton.innerHTML = currentQuestion.choices[i]
                 answerButton.setAttribute("data-id", currentQuestion.choices[i])
                 buttonBoxEl.appendChild(answerButton);
@@ -67,11 +73,10 @@ $(document).ready(function () {
         }
 
         else {
-            stopTimer()
             stopQuiz()
         }
 
-
+        //answer buttons click event
         $(".answerbtn").click(function () {
 
             var selected = $(this).attr("data-id")
@@ -94,9 +99,44 @@ $(document).ready(function () {
         });
 
     }
+    //score form submit click 
+    $("#submit").click(function (event) {
+        generateScores()
+    });
 
+    //Function to populate scores
+    function generateScores() {
+        highscores = JSON.parse(localStorage.getItem("scores")) || [];
+
+        highscores.unshift({
+            name: scoreName.value,
+            score: score,
+        });
+
+        localStorage.setItem("scores", JSON.stringify((highscores)))
+
+        formBoxEl.setAttribute("class", "hide")
+        questionEl.innerText = "Can you beat your high score?"
+        scoreBoxEl.removeAttribute("class", "hide")
+        scoreBoxEl.setAttribute("class", "col-8 text-center")
+        for (var j = 0; j < highscores.length; j++) {
+            var highscores = JSON.parse(localStorage.getItem("scores")) || [];
+            $("#yourScores").append('<li class="list-group-item">' + "Name: " + highscores[j].name + "  Score: " + highscores[j].score + '</li>')
+        }
+
+    }
+
+    //Highscores nav link click event
+    highscoresEl.addEventListener("click", function () {
+        titleEl.setAttribute("class", "hide")
+        $("#button-box").empty();
+        generateScores()
+    })
+
+    //Clear Scores button click event
+    $("#clearScore").click(function () {
+        localStorage.clear()
+        // highscores = ""
+        $("#yourScores").empty()
+    })
 })
-
-//highscores page is loaded so high score can be viewed.
-//create README
-//** Bonus: Add light mode dark mode switch **/
